f027f7b63cae442c52ff4c9a83e5e440a8500b85
Dilip-kumar-96 <120187622+Dilip-kumar-96@users.noreply.github.com>
2025-03-06 20:30:07 +0530

    add create employee endpoint

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from flask import Flask
 import psycopg2
 
 conn = psycopg2.connect(
@@ -6,12 +7,18 @@ conn = psycopg2.connect(
     password="dilip1231",
     host="localhost"
 )
-cursor = conn.cursor()
+@app.route("/create/employee_batch", methods=['POST']) #Send json data in the form of list
+def create_employee():
+    cursor = conn.cursor()
+    data = request.get_json()
+    insert_query = "insert into company_directory.employee_data (employee_id, manager_id, employee_name, manager_name, salary) values (%s, %s, %s, %s, %s)"
+    values_list = []
+    for i in data:
+        values_list.append((i['employee_id'], i['manager_id'], i['employee_name'], i['manager_name'], i['salary']))
 
-cursor.execute("SELECT * from learning.persons;")
+    cursor.executemany(insert_query, values_list)
+    conn.commit()
+    cursor.close()
 
-# Fetch all rows from database
-record = cursor.fetchall()
-
-print("Data from Database:- ", record)
- print ("created git repository")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
