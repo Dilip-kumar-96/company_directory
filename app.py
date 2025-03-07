8971f2f2ed9140ca44d754e54b0b3174d8993610
Dilip-kumar-96 <120187622+Dilip-kumar-96@users.noreply.github.com>
2025-03-07 19:13:52 +0530

    pushing new branch to github

--- app.py
+++ app.py
@@ -36,5 +36,18 @@ def create_employee():
     cursor.close()
     return jsonify({"message": "employee created successfully!"}), 201
 
+@app.route("/create/employee_batch", methods=['POST']) #Send json data in the form of list
+def create_employee():
+    cursor = conn.cursor()
+    data = request.get_json()
+    insert_query = "insert into company_directory.employee_data (employee_id, manager_id, employee_name, manager_name, salary) values (%s, %s, %s, %s, %s)"
+    values_list = []
+    for i in data:
+        values_list.append((i['employee_id'], i['manager_id'], i['employee_name'], i['manager_name'], i['salary']))
+
+    cursor.executemany(insert_query, values_list)
+    conn.commit()
+    cursor.close()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
